da74b7bb1b53c61654bf05b8a2153459ea681445
valenzsa <valenzsa418@gmail.com>
2023-01-31 22:45:53 -0500

    Created Enums of PlantTypes and Vendors Models

---
+++ SeedCatalogClassLibrary/Models/VendorsModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static SeedCatalogClassLibrary.Enums.Enums;
+
+namespace SeedCatalogClassLibrary.Models
+{
+    public class VendorsModel
+    {
+        public string VendorsName { get; set; }
+        public string VendorsWebsiteURL { get; set; }
+        public List<PlantTypes> PlantType { get; set; }
+    }
+}
