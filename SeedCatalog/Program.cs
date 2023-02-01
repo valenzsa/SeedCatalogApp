6c38c8ffcc4a431aab4579beff972c5a8d256f8e
valenzsa <valenzsa418@gmail.com>
2023-01-31 23:50:00 -0500

    Create WelcomeMessage and CreateVendor in program.cs file

--- SeedCatalog/Program.cs
+++ SeedCatalog/Program.cs
@@ -1,8 +1,10 @@
-﻿using System;
+﻿using SeedCatalogClassLibrary.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static SeedCatalogClassLibrary.Enums.Enums;
 
 namespace SeedCatalog
 {
@@ -10,8 +12,63 @@ namespace SeedCatalog
     {
         static void Main(string[] args)
         {
+            WelcomeMessage();
+            CreateVendor();
 
             Console.ReadLine();
+        }
+
+        public static void WelcomeMessage()
+        {
+            Console.WriteLine("Welcome to my Seed Catalog");
+            Console.Write("==========================");
+            Console.WriteLine();
+        }
+
+        public static void CreateVendor()
+        {
+            List<VendorsModel> vendors = new List<VendorsModel>();
+            string vendorsName = string.Empty;
+            string plantType = string.Empty;
+
+            do
+            {
+                VendorsModel vendor = new VendorsModel();
+
+                Console.Write("What is the Vendor's Name? ");
+                vendorsName = Console.ReadLine();
+                vendor.VendorsName = vendorsName;
+
+                Console.Write("What is the Vendor's Website URL? ");
+                vendor.VendorsWebsiteURL = Console.ReadLine();
+
+                do
+                {
+                    Console.Write("Select the Plant Type (Flowers or Vegetables): ");
+                    plantType = Console.ReadLine();
+                } while (plantType.ToLower() != "flowers" && plantType.ToLower() != "vegetables");
+                
+                
+                
+                // Loop through PlantTypes enums and compare with user's plant type input
+                foreach(string planttype in Enum.GetNames(typeof(PlantTypes)))
+                {
+                    if(plantType.ToLower() == planttype.ToLower())
+                    {
+                        if(planttype.ToLower() == "flowers")
+                        {
+                            Console.WriteLine($"You have selected {planttype}");
+                        } else if(planttype.ToLower() == "vegetables")
+                        {
+                            Console.WriteLine($"You have selected {planttype}");
+                        } 
+                    }
+                }
+                
+                // Add vendor to a list of vendors
+                vendors.Add(vendor);
+
+            } while (vendorsName.ToLower() != "exit");
         }
     }
 }
