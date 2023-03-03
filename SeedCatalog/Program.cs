3a78e067dd638ee3840f49978bc9a917e851f1b1
valenzsa <valenzsa418@gmail.com>
2023-03-02 23:29:17 -0500

    Moved CreateDetails inside IDetails for reusability.  Both FlowerModel and VegetableModel inherits IDetails.

--- SeedCatalog/Program.cs
+++ SeedCatalog/Program.cs
@@ -62,29 +62,58 @@ namespace SeedCatalog
                     {
                         if(planttype.ToLower() == "flowers")
                         {
+                            FlowerModel flower = new FlowerModel();
+
                             Console.WriteLine(); // Spacing purposes
                             Console.WriteLine($"You have selected {planttype}");
                             Console.WriteLine(); // Spacing purposes
 
-                            Console.WriteLine($"Please select a category name below at which your flower seed belongs to: (or type 'Exit' to start over) ");
-                            Console.WriteLine(); // Spacing purposes
-
-
-                            // Loop through Vegetable Categories and display on screen
-                            foreach (string flowerCategories in Enum.GetNames(typeof(FlowerCategories)))
+                            // Checking if typed category name matches the category in the list
+                            do
                             {
-                                Console.WriteLine($"{flowerCategories}");
-                            }
+                                Console.WriteLine($"Please select a category name below at which your vegetable seed belongs to: (or type 'Exit' to start over) ");
+                                Console.WriteLine(); // Spacing purposes
 
-                            Console.WriteLine(); // Spacing purposes
-                            categoryName = Console.ReadLine();
-                            Console.WriteLine(); // Spacing purposes
+                                // Loop through Vegetable Categories and display on screen
+                                foreach (string flowerCategoriesList in Enum.GetNames(typeof(FlowerCategories)))
+                                {
+                                    Console.WriteLine($"{flowerCategoriesList}");
+                                }
+
+                                Console.WriteLine(); // Spacing purposes
+                                categoryName = Console.ReadLine();
+                                Console.WriteLine(); // Spacing purposes
+
+                                // Loop through Vegetable Categories and display on screen
+                                foreach (string flowerCategories in Enum.GetNames(typeof(FlowerCategories)))
+                                {
+                                    // Checks if selected vegetable name exists in the list of vegetable categories.
+                                    if (categoryName.ToLower() == flowerCategories.ToLower())
+                                    {
+                                        //Console.WriteLine($"{vegetableCategoryName} is EQUAL TO {vegetableCategories}");
+                                        isSelectedCategoryNameMatched = true;
+                                        //CreateDetails(categoryName);
+
+                                        flower.CreateDetails(categoryName);
+                                    }
+                                    else
+                                    {
+                                        //Console.WriteLine($"{vegetableCategoryName} is NOT EQUAL TO {vegetableCategories}");
+
+                                        isSelectedCategoryNameMatched = false;
+                                    }
+                                }
+                            } while (isSelectedCategoryNameMatched != true && categoryName.ToLower() != "exit");
+
                             Console.WriteLine("==========================");
                             Console.WriteLine(); // Spacing purposes
 
 
+
                         } else if(planttype.ToLower() == "vegetables")
                         {
+                            VegetableModel vegetable = new VegetableModel();
+
                             Console.WriteLine(); // Spacing purposes
                             Console.WriteLine($"You have selected {planttype}");
                             Console.WriteLine(); // Spacing purposes
@@ -114,6 +143,8 @@ namespace SeedCatalog
                                         //Console.WriteLine($"{vegetableCategoryName} is EQUAL TO {vegetableCategories}");
                                         isSelectedCategoryNameMatched = true;
                                         //CreateDetails(categoryName);
+                                        
+                                        vegetable.CreateDetails(categoryName);
                                     }
                                     else
                                     {
--- SeedCatalogClassLibrary/Models/FlowerModel.cs
+++ SeedCatalogClassLibrary/Models/FlowerModel.cs
@@ -20,5 +20,53 @@ namespace SeedCatalogClassLibrary.Models
         public string Instructions { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
         public bool hasImages { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
         public string Images { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public void CreateDetails(string categoryName)
+        {
+            List<FlowerModel> flowers = new List<FlowerModel>();
+            string response;
+
+            Console.Write($"Add flower to {categoryName} category? Type 'Yes' or 'No': ");
+            response = Console.ReadLine();
+
+            if (response.ToLower() == "yes")
+            {
+                do
+                {
+                    FlowerModel flower = new FlowerModel();
+
+                    Console.Write("Name: ");
+                    flower.Name = Console.ReadLine();
+
+                    Console.Write("Description: ");
+                    flower.Description = Console.ReadLine();
+
+                    Console.Write("Genus: ");
+                    flower.Genus = Console.ReadLine();
+
+                    Console.Write("Species: ");
+                    flower.Species = Console.ReadLine();
+
+                    Console.Write("Light Requirements (ie: Full Sun, Partial Sun, Shade, etc.): ");
+                    flower.LightRequirements = Console.ReadLine();
+
+                    Console.Write("Zone: (6a, 6b, 8a, 10b etc.) ");
+                    flower.Zone = Console.ReadLine();
+
+                    Console.Write("Instructions: ");
+                    flower.Instructions = Console.ReadLine();
+
+                    Console.Write("Images: ");
+                    flower.Images = Console.ReadLine();
+
+                    flowers.Add(flower);
+
+                    Console.Write($"Add another flower to {categoryName} category? Type 'Yes' or 'No': ");
+                    response = Console.ReadLine();
+                    Console.WriteLine(); // Spacing purposes
+
+                } while (response.ToLower() != "no");
+            }
+        }
     }
 }
